a108ddb0966aa85c647b92d588306f5a9e169c19
iB0GI <marko.bogovic20@gmail.com>
2026-06-22 16:19:16 +0200

    update state to include pokeapi

--- src/state.ts
+++ src/state.ts
@@ -1,14 +1,18 @@
 import { createInterface, type Interface } from "readline";
 import { getCommands } from "./commands.js";
+import { PokeAPI } from "./pokeapi.js";
 export type CLICommand = {
   name: string;
   description: string;
-  callback: (state: State) => void;
+  callback: (state: State) => Promise<void>;
 };
 
 export type State = {
   readline: Interface;
   commands: Record<string, CLICommand>;
+  apiClient: PokeAPI;
+  nextLocationUrl: string;
+  prevLocationUrl: string;
 };
 
 export function initState(): State {
@@ -20,5 +24,8 @@ export function initState(): State {
   return {
     readline: rl_interface,
     commands: getCommands(),
+    apiClient: new PokeAPI(),
+    nextLocationUrl: "",
+    prevLocationUrl: "",
   };
 }
