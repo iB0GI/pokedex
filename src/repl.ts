c1c2d16a80080a1caffc951e6186494c5fe33f81
iB0GI <marko.bogovic20@gmail.com>
2026-06-22 14:10:18 +0200

    add repl interface with commands exit and help

--- src/repl.ts
+++ src/repl.ts
@@ -1,5 +1,45 @@
+import { createInterface } from "readline";
+import { getCommands } from "./commands.js";
+
 export function cleanInput(input: string): string[] {
   const inputTrimmed = input.trim().toLowerCase();
   const words = inputTrimmed.split(/\s+/);
   return words;
+}
+
+export function startREPL() {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+
+  rl.prompt();
+
+  rl.on("line", async (input) => {
+    const words = cleanInput(input);
+    if (words.length === 0) {
+      rl.prompt();
+      return;
+    }
+
+    const commandName = words[0];
+
+    const commands = getCommands();
+    const cmd = commands[commandName];
+    if (!cmd) {
+      console.log(
+        `Unknown command: "${commandName}". Type "help" for a list of commands.`,
+      );
+      rl.prompt();
+      return;
+    }
+
+    try {
+      cmd.callback(commands);
+    } catch (e) {
+      console.log(e);
+    }
+    rl.prompt();
+  });
 }
